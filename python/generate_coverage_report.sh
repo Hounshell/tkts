9132f50541c8bfdcd129bc65ed59cae7b349909d
Paul Hounshell <paul@hounshell.com>
2026-08-08 01:46:59 -0700

    Trim coverage report to just run tests

--- python/generate_coverage_report.sh
+++ python/generate_coverage_report.sh
@@ -1,5 +1,5 @@
 # Generate coverage data.
-bazel coverage --combined_report=lcov --instrumentation_filter="^//python/src" //python/...
+bazel coverage --combined_report=lcov --instrumentation_filter="^//python/src" //python/test/tkts
 
 # Generate a coverage report.
 genhtml -o reports/python/coverage "$(bazel info output_path)/_coverage/_coverage_report.dat"
